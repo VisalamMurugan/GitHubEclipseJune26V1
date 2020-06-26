992fa951ad55d6417cf00a822b6940978380e377
VisalamMurugan <VisalamMurugan>
2020-06-26 14:27:02 +0530

    V1.0

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/* HelloWorld.java
+ */
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+
+
+		System.out.println("Welcome to java ,base & Release master!!!");
+		System.out.println("Eclipse with Github Integration  Thank you!!!");
+
+	}
+}
